62d97457f7ff82ff18ce9ca1c77d9d89443f5767
Alex Law <agent_law@hotmail.com>
2017-05-09 17:00:12 +0100

    Test is not divisible by 15

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -32,6 +32,10 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
     });
 
+    it('divisible by 15', function() {
+      expect(fizzbuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+
   });
 
 });
