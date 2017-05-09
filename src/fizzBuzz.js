95fd4d337e6b324fd5bdf7e65664e4eadbef6931
Alex Law <agent_law@hotmail.com>
2017-05-09 17:10:22 +0100

    Refactorised Fizzbuzz methods

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,19 +1,29 @@
 var Fizzbuzz = function(){};
+// Fizzbuzz.prototype.isDivisibleByThree = function (number) {
+//     return (number % 3 === 0);
+// };
+//
+// Fizzbuzz.prototype.isDivisibleByFive = function (number) {
+//   return (number % 5 === 0);
+// };
+
+// Fizzbuzz.prototype.isDivisibleByFifteen = function (number) {
+//   return (number % 15 === 0);
+// };
+
+Fizzbuzz.prototype._isDivisibleBy = function (number, divisor) {
+  return (number % divisor === 0);
+};
+
 Fizzbuzz.prototype.isDivisibleByThree = function (number) {
-    return (number % 3 === 0);
+    return this._isDivisibleBy(number, 3);
 };
 
 Fizzbuzz.prototype.isDivisibleByFive = function (number) {
-  return (number % 5 === 0);
+  return this._isDivisibleBy(number, 5);
 };
 
-Fizzbuzz.prototype.isDivisibleByFifteen = function (number) {
-  return (number % 15 === 0);
+
+Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
 };
-// Fizzbuzz.prototype._isDivisibleBy = function (number, divisor) {
-//   return (number % divisor === 0);
-// };
-//
-// Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
-//   return _isDivisibleBy(number, 15);
-// };
